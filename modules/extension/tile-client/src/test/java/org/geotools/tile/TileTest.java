b4a82762493d3200c05e6e90abd28e176255ab1b
Roar Brænden <roar.brenden@me.com>
2021-03-08 15:51:12 +0100

    Custom tile test

--- modules/extension/tile-client/src/test/java/org/geotools/tile/TileTest.java
+++ modules/extension/tile-client/src/test/java/org/geotools/tile/TileTest.java
@@ -20,6 +20,7 @@ import java.awt.image.BufferedImage;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.http.MockHttpClient;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.geotools.tile.impl.WebMercatorZoomLevel;
 import org.geotools.tile.impl.ZoomLevel;
@@ -28,6 +29,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.locationtech.jts.geom.Envelope;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 public class TileTest {
 
@@ -35,7 +37,7 @@ public class TileTest {
 
     @Before
     public void beforeTest() {
-        this.tile = createTestTile();
+        this.tile = new CustomTile();
     }
 
     @After
@@ -55,7 +57,7 @@ public class TileTest {
         Assert.assertNotEquals(null, this.tile);
         Assert.assertNotEquals("Blah", this.tile);
 
-        Tile otherTile = createTestTile();
+        Tile otherTile = new CustomTile();
         Assert.assertEquals(this.tile, otherTile);
         Assert.assertEquals(otherTile, this.tile);
     }
@@ -80,25 +82,61 @@ public class TileTest {
         Assert.assertEquals("SomeService_01234xy56789", this.tile.getId());
     }
 
-    protected Tile createTestTile() {
+    protected static class CustomTile extends Tile {
 
-        ReferencedEnvelope env =
-                new ReferencedEnvelope(new Envelope(6, 15, 47, 55), DefaultGeographicCRS.WGS84);
-        ZoomLevel zoomLevel = new WebMercatorZoomLevel(5);
+        public CustomTile() {
+            super(
+                    TileIdentifierTest.createTileIdentifierPrototype(
+                            new WebMercatorZoomLevel(5), 10, 12, "SomeService"),
+                    new ReferencedEnvelope(new Envelope(6, 15, 47, 55), DefaultGeographicCRS.WGS84),
+                    256,
+                    new CustomService("http://test.tile-service.com/"));
+        }
 
-        return new Tile(
-                TileIdentifierTest.createTileIdentifierPrototype(zoomLevel, 10, 12, "SomeService"),
-                env,
-                256) {
-
-            @Override
-            public URL getUrl() {
-                try {
-                    return new URL("http://localhost/service/tile123");
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException(e);
-                }
+        @Override
+        public URL getUrl() {
+            try {
+                return new URL(service.getBaseUrl() + "tile123");
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
             }
-        };
+        }
+    }
+
+    protected static class CustomService extends TileService {
+
+        protected CustomService(String baseURL) {
+            super("Custom tile service", baseURL, new MockHttpClient());
+        }
+
+        @Override
+        public double[] getScaleList() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ReferencedEnvelope getBounds() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public CoordinateReferenceSystem getProjectedTileCrs() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public TileFactory getTileFactory() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Tile findTileAtCoordinate(double lon, double lat, ZoomLevel zoomLevel) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ZoomLevel getZoomLevel(int zoomLevel) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
