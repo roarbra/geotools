5bd0a40b8625f5ce31942f0aa3eaf09ec50f33f4
Roar Brænden <roar.brenden.no@gmail.com>
2024-12-12 08:52:01 +0100

    Add logging to AbstractComplexBinding

--- modules/extension/xsd/xsd-core/src/main/java/org/geotools/xsd/AbstractComplexBinding.java
+++ modules/extension/xsd/xsd-core/src/main/java/org/geotools/xsd/AbstractComplexBinding.java
@@ -17,8 +17,11 @@
 package org.geotools.xsd;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.namespace.QName;
 import org.eclipse.xsd.XSDElementDeclaration;
+import org.geotools.util.logging.Logging;
 import org.picocontainer.MutablePicoContainer;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -29,17 +32,32 @@ import org.w3c.dom.Element;
  * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
  */
 public abstract class AbstractComplexBinding implements ComplexBinding {
+
+    private static final Logger LOGGER = Logging.getLogger(AbstractComplexBinding.class);
+
     /** Does nothing, subclasses should override this method. */
     @Override
     public void initializeChildContext(
             ElementInstance childInstance, Node node, MutablePicoContainer context) {
         // does nothing, subclasses should override
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine(
+                    "abstract class AbstractComplexBinding does nothing, "
+                            + this.getClass().getName()
+                            + " should override initializeChildContext");
+        }
     }
 
     /** Does nothing, subclasses should override this method. */
     @Override
     public void initialize(ElementInstance instance, Node node, MutablePicoContainer context) {
         // does nothing, subclasses should override
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine(
+                    "abstract class AbstractComplexBinding does nothing, "
+                            + this.getClass().getName()
+                            + " should override initialize");
+        }
     }
 
     /**
@@ -57,6 +75,12 @@ public abstract class AbstractComplexBinding implements ComplexBinding {
      */
     @Override
     public Object parse(ElementInstance instance, Node node, Object value) throws Exception {
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine(
+                    "abstract class AbstractComplexBinding does nothing, "
+                            + this.getClass().getName()
+                            + " should override parse");
+        }
         return null;
     }
 
@@ -68,6 +92,12 @@ public abstract class AbstractComplexBinding implements ComplexBinding {
      */
     @Override
     public Element encode(Object object, Document document, Element value) throws Exception {
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine(
+                    "abstract class AbstractComplexBinding does nothing, "
+                            + this.getClass().getName()
+                            + " should override encode");
+        }
         return value;
     }
 
@@ -80,6 +110,12 @@ public abstract class AbstractComplexBinding implements ComplexBinding {
     @Override
     public Object getProperty(Object object, QName name) throws Exception {
         // do nothing, subclasses should override
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine(
+                    "abstract class AbstractComplexBinding does nothing, "
+                            + this.getClass().getName()
+                            + " should override getProperty");
+        }
         return null;
     }
 
@@ -94,6 +130,12 @@ public abstract class AbstractComplexBinding implements ComplexBinding {
     public List<Object[]> getProperties(Object object, XSDElementDeclaration element)
             throws Exception {
         // do nothing, subclasses should override
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine(
+                    "abstract class AbstractComplexBinding does nothing, "
+                            + this.getClass().getName()
+                            + " should override getProperties");
+        }
         return null;
     }
 }
